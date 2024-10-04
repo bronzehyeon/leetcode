66af4439abd0a2e3c4cf5be034ae8ff2c882da8f
황동현 <106989359+bronzehyeon@users.noreply.github.com>
2024-10-04 19:59:58 +0900

    Time: 1258 ms (6.88%), Space: 292 MB (15.59%) - LeetHub

---
+++ 0015-3sum/0015-3sum.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int N = nums.size();
+        vector<vector<int>> v;
+        set<vector<int>> s;
+        sort(nums.begin(), nums.end());
+
+        for(int i = 0; i < N; i++){
+            int j = i + 1;
+            int k = nums.size() - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0) {
+                    s.insert({nums[i], nums[j], nums[k]});
+                    j++;
+                    k--;
+                } else if (sum < 0) {
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+        }
+
+        auto iter = s.begin();
+        while(iter != s.end()){
+            v.push_back(*iter);
+            iter++;
+        }
+        return v;
+    }
+};
